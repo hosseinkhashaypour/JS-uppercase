be52b4d4f205fd8864dd5cfdab7885120b6ff7f9
hosseinkhashaypour <154625781+hosseinkhashaypour@users.noreply.github.com>
2024-01-05 12:51:18 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// let text = "ABCDEFGHI"
+// document.getElementById("tamrin").innerHTML = text.length;
+// let text = "hello JavaScript"
+// document.getElementById("tamrin").innerHTML = text.charAt(0)
+// const Myname = "Easy leran"
+// let letter = Myname.charAt(0) 
+// document.getElementById("tamrin").innerHTML = letter
+// let text = "APPLE, bannana, kiwi";
+// let part = text.slice(0, 10);
+// document.getElementById("tamrin").innerHTML = part;
+const orgstring = "hosseinkhashaypour"
+const changestring = orgstring[0].toLocaleUpperCase() + orgstring.slice(1)
+console.log(orgstring); //name
+console.log(changestring); //Name
